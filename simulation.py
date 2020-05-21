86868a8691fdf0318a7bfa82f09b35e53f962629
thomas.garello <thomas.garello@fhnw.ch>
2020-05-21 21:47:15 +0200

    add simulation script which initiate a Femm instance

---
+++ simulation.py
@@ -0,0 +1,19 @@
+import femm
+
+femm.openfemm(1)  # create an instance of femm without GUI
+
+# problem definition
+
+# geometry definition
+
+# boundaries conditions
+
+# materials properties
+
+# meshing
+
+# analysis
+
+# result data export
+
+femm.closefemm()  # close the instance of femm
